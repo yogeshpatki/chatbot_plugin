5c2a1d4c331e477ecd94dc3f9287c1b7ca2dac00
ankitm2210 <ankit.mishra100@gmail.com>
2017-03-08 22:48:20 +0530

    "content.js changes""

--- js/content.js
+++ js/content.js
@@ -4,21 +4,11 @@ chrome.runtime.onMessage.addListener(
     if( request.message === "clicked_browser_action" ) {
     	if(request.url.indexOf("accountSummary") >= 0){
     		var data = getData();
-    		loadContext(data);
+    		loadContext(data);	
     	}
 		$('body').append('<div class="chatBot" id="chatBot"> <span id="chatMessage">Click Here To Chat</span></div>');
 		bindEvent();
-		$('#accountTile').attr('data-step',1);
-		$('#accountTile').attr('data-intro','Your balance is displayed here');
-		$('#accountTile').attr('data-position','right');
-		$('#paymentTile').attr('data-step',2);
-		$('#paymentTile').attr('data-intro','This displays your current payment due.');
-		$('#paymentTile').attr('data-position','right');
-		$('#activityTile').attr('data-step',3);
-		$('#activityTile').attr('data-intro','These are  you recent activities.');
-		$('#activityTile').attr('data-position','right');
-
-
+ 	
     }
   }
 );
@@ -45,14 +35,85 @@ var loadContext = function(context){
 var bindEvent = function(){ $('.chatBot').on('click',function(){
 	var that = $(this);
 	that.css("height","400px");
-	that.html('<iframe class="chatWindow card1" src="https://fuzzychat.herokuapp.com/"> </iframe>');
+	that.html('<iframe class="chatWindow card1" src="http://localhost:8080/"> </iframe>');
             
 
 });
 }
 
+function startAccountSummaryTour(){
+	var intro = introJs();
+	intro.setOptions(getTourSteps().accountSummaryTourSteps);
+	intro.onchange(function(data){
+		console.log(data);
+	});
+	intro.start();
+}
+
+function startBenefitCenterTour(){
+	var intro = introJs();
+	intro.setOptions(getTourSteps().rewardsHubSteps);
+	intro.onchange(function(data){
+		console.log(data);
+	});
+	intro.start();
+}
+
+function startTour(tourName) {
+	if(tourName == "accountsummary"){
+		startAccountSummaryTour();
+	}else if(tourName == "rewardsHub"){
+		startBenefitCenterTour();
+	}
+}
 window.onmessage = function(e){
-    if (e.data == 'tourNeeded') {
-        introJs().start();
+    if (e.data.tourNeeded) {
+    	startTour(e.data.tourName);   
     }
 };
+
+function getTourSteps(){
+var tourSteps = {
+	accountSummaryTourSteps : {
+		steps : [
+		  {
+            element: '#accountTile',
+            intro: "Your balance is displayed here",
+            position: 'right'
+          },
+          {
+            element: '#paymentTile',
+            intro: 'This displays your current payment due.',
+            position: 'left'
+          },
+          {
+            element: '#activityTile',
+            intro: "These are  you recent activities.",
+            position: 'right'
+          }
+		
+		]
+	},
+	rewardsHubSteps : {
+		steps : [
+		  {
+            element: document.querySelectorAll('.benefits-center-tile.row')[0],
+            intro: "This is your Rewards Summary in a nutshell.",
+            position: 'top'
+          },
+          {
+            element: document.querySelectorAll('.reward-tile-container.tile')[0],
+            intro: 'You can click this to see how to earn rewards.',
+            position: 'right'
+          },
+          {
+            element: document.querySelectorAll('.reward-tile-container.tile')[1],
+            intro: "This will help you understand how to use your rewards.",
+            position: 'left'
+          }
+		
+		]	
+	} 
+}
+return tourSteps;
+}
